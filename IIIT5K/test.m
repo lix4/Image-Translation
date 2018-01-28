c5d82f4c9f513ec8e21a3e38e731b3211e48da58
Xiwen Li <lix4@rose-hulman.edu>
2018-01-28 18:57:20 -0500

    Commit Script for Preprocessing

--- IIIT5K/test.m
+++ IIIT5K/test.m
@@ -1,4 +1,31 @@
-load("traindata.mat");
+load("testdata.mat");
+load("testCharBound.mat");
+% load("traindata.mat");
+% load("trainCharBound.mat");
 
 %%
-traindata(1)
+k = 10
+testCharBound(k);
+imshow(imread(testCharBound(k).ImgName));
+
+%%
+xData = {};
+yData = {};
+for k = 1:size(traindata, 2)
+    name = traindata(k).ImgName
+    img = imread(name);
+    gray_img = img;
+    if (size(img, 3) > 1)
+        gray_img = rgb2gray(img);
+    end
+    gray_img = imresize(gray_img, [32, 32]);
+    xData = [xTest, gray_img];
+    yData = [yTest, traindata(k).GroundTruth];
+end
+
+%%
+save('./xTest.mat', 'xTest');
+save('./yTest.mat', 'yTest');
+% save('./xTrain.mat', 'xTrain');
+% save('./yTrain.mat', 'yTrain');
+
